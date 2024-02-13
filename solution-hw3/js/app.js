ae322859bca774a0efd845d343a4428c9811d9cc
elzlee <elizabethlee245@gmail.com>
2024-02-13 15:22:26 -0500

    hw3 epsilon rounding

--- solution-hw3/js/app.js
+++ solution-hw3/js/app.js
@@ -42,7 +42,7 @@ const glazing4 = ('Double Chocolate', 1.50)
 class PackSize {
     constructor (option, priceAdaptation){
         this.option = option;
-        this.priceAdaptaion = priceAdaptation;
+        this.priceAdaptation = priceAdaptation;
     }
 }
 const packsize1 = (1, 1)
@@ -72,7 +72,7 @@ function glazingChange(selectElement, rollnum) {
     thisRoll.glazingPriceAdaptation = parseFloat(selectElement.value);
     let adjustedPrice = (thisRoll.basePrice + thisRoll.glazingPriceAdaptation) 
                             * thisRoll.sizePriceAdaptation;
-    thisRoll.adjustedPrice = Math.round((adjustedPrice) * 100) / 100; 
+    thisRoll.adjustedPrice = Math.round((adjustedPrice + Number.EPSILON) * 100) / 100; 
     console.log(thisRoll.glazing);
     console.log(thisRoll.glazingPriceAdaptation);
     console.log(thisRoll.adjustedPrice);
@@ -86,7 +86,7 @@ function sizeChange(selectElement, rollnum) {
     thisRoll.sizePriceAdaptation = parseFloat(selectElement.value);
     let adjustedPrice = (thisRoll.basePrice + thisRoll.glazingPriceAdaptation) 
                             * thisRoll.sizePriceAdaptation;
-    thisRoll.adjustedPrice = Math.round(adjustedPrice*100)/100;
+    thisRoll.adjustedPrice = Math.round((adjustedPrice+ Number.EPSILON)*100)/100;
     console.log(thisRoll.size);
     console.log(thisRoll.sizePriceAdaptation);
     console.log(thisRoll.adjustedPrice);
