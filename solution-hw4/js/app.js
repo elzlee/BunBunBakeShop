4a6959c97ba8f4ac803f9170e0dff40bede3be31
elzlee <elizabethlee245@gmail.com>
2024-02-20 20:14:55 -0500

    url params lead to correct pages

--- solution-hw4/js/app.js
+++ solution-hw4/js/app.js
@@ -1,7 +1,6 @@
 /* (1) Create a Roll object that stores its type, price, glazing and pack options. 
 price = current price, could be updated */
 
-
 class Roll {
     constructor (type, basePrice, glazing, size){
         this.type = type;
@@ -11,8 +10,19 @@ class Roll {
         this.size = size;
         this.sizePriceAdaptation = size;
         this.adjustedPrice = basePrice;
+        //this.updateElement();
     }
+        /*
+    updateElement() {
+        const noteImageElement = this.element.querySelector('.notecard-thumbnail');
+        const noteTitleElement = this.element.querySelector('.note-title');  
+
+        noteImageElement.src = this.noteImageURL;
+        noteTitleElement.innerText = this.noteTitle;
+    }
+*/
 }
+
 /* initializing the Rolls */
 const roll1 = new Roll("originalRoll", 2.49, "keeporiginal", 1);
 const roll2 = new Roll("appleRoll", 3.49, "keeporiginal", 1);
@@ -21,7 +31,7 @@ const roll4 = new Roll("walnutRoll", 3.49, "keeporiginal", 1);
 const roll5 = new Roll ("chocRoll", 3.99, "keeporiginal", 1);
 const roll6 = new Roll ("strawberryRoll", 2.49, "keeporiginal", 1);
 
-const rolls = [roll1, roll2, roll3, roll4, roll5, roll6];
+const oldRollsArr = [roll1, roll2, roll3, roll4, roll5, roll6];
 
 
 /* (2) Add JavaScript objects to represent price adaptations based on user selections. */
@@ -41,19 +51,21 @@ const packPrices = {
 
 
 /* (3.1) Populate glazing options with corresponding price adaptation values */
-const glazingSelect = document.querySelector("select#glazing-options");
+const glazingSelect = document.getElementById("glazing-options");
 
 for (const [glazingOption, priceAdaptation] of Object.entries(glazingPrices)) {
   const option = document.createElement("option");
+  console.log("opption="+option)
   option.textContent = glazingOption;
   option.value = priceAdaptation;
+  console.log("glazingSelect=" + glazingSelect)
   glazingSelect.appendChild(option);
   /* appendChild: adds a node to the end of the list of children 
                   of a specified parent node.*/
 }
 
 /* (3.2) Populate pack options with corresponding price adaptation values */
-const packSelect = document.querySelector("select#pack-options");
+const packSelect = document.getElementById("pack-options");
 
 for (const [packOption, priceAdaptation] of Object.entries(packPrices)) {
   const option = document.createElement("option");
@@ -65,7 +77,7 @@ for (const [packOption, priceAdaptation] of Object.entries(packPrices)) {
 
 /* (4) Compute new price */
 function glazingChange(selectElement, rollnum) {
-    let thisRoll=rolls[rollnum-1]; /* for when there are multiple detail pages */
+    let thisRoll=oldRollsArr[rollnum-1]; /* for when there are multiple detail pages */
     thisRoll.glazing = selectElement.options[selectElement.selectedIndex].text;
     thisRoll.glazingPriceAdaptation = parseFloat(selectElement.value);
     let adjustedPrice = (thisRoll.basePrice + thisRoll.glazingPriceAdaptation) 
@@ -79,7 +91,7 @@ function glazingChange(selectElement, rollnum) {
 }
 
 function packChange(selectElement, rollnum) {
-    let thisRoll=rolls[rollnum-1]; /* for when there are multiple detail pages */
+    let thisRoll=oldRollsArr[rollnum-1]; /* for when there are multiple detail pages */
     thisRoll.size = selectElement.options[selectElement.selectedIndex].text;
     thisRoll.sizePriceAdaptation = parseFloat(selectElement.value);
     let adjustedPrice = (thisRoll.basePrice + thisRoll.glazingPriceAdaptation) 
@@ -91,4 +103,32 @@ function packChange(selectElement, rollnum) {
     document.getElementById("adjustedPriceShown").innerHTML = "$" + thisRoll.adjustedPrice;
   
 }
+/* ------------------------ CART -------------------------- */
+const cart = [];
+
+
+/* ------------------------ URL SEARCH PARAMS -------------------------- */
+
+// Get query string from URL. This is the list of params that begin with a ? mark. 
+const queryString = window.location.search;
+console.log("queryString = " + queryString);
+
+//Use the query string to create a URLSearchParams object
+const params = new URLSearchParams(queryString);
+console.log("params = " + params);
+
+//Access the param we want using "get" method 
+const chosenRoll = params.get('roll');
+console.log("chosenRoll = " + chosenRoll);
+
+/* ------------------------ UPDATE HTML GIVEN URL PARAMS -------------------------- */
+
+const headerHTML = document.querySelector('#banner');
+headerHTML.innerText = chosenRoll + ' Cinnamon Roll';
+
+const imgHTML = document.querySelector('.product-image');
+imgHTML.src = "images/products/" + rolls[chosenRoll]["imageFile"];
+
+const priceHTML = document.querySelector('#adjustedPriceShown');
+priceHTML.innertext = "$" + rolls[chosenRoll].basePrice;
 
